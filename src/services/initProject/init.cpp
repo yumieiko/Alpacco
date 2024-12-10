c94992a46010b8d48dc6c7687b4dadf95a69ffaf
yumieiko <r3s1zetv@gmail.com>
2024-12-10 19:47:27 +0000

    remove default depend

--- src/services/initProject/init.cpp
+++ src/services/initProject/init.cpp
@@ -14,8 +14,8 @@ void InitProject::SetupVenv(const char* venvname)
     std::string _venvnamestr = (venvname);
     std::string _venvinit = (" python -m venv " + _venvnamestr + " ");
     std::string command = _venvinit;
-    spdlog::info("Setup virt env...");
-    std::cout << command << std::endl;
+    spdlog::info("Creating Virtual env");
+    spdlog::debug("Creating Python VENV With '" + command + "'");
     system(command.c_str());
 }
 
@@ -25,23 +25,21 @@ void InitProject::CreateConfig(const char* projectname)
     Json::Value root;
     spdlog::info("Creating config file");
     try {
-        spdlog::info("Formatting pretty-json");
+        spdlog::debug("Formatting pretty-json");
         root["projectname"] = projectname;
         Json::Value dependencies(Json::arrayValue);
-        dependencies.append("path");
-        dependencies.append("path2");
         root["dependencies"] = dependencies;
         Json::StreamWriterBuilder builder;
         builder["indentation"] = "    ";
         Json::StreamWriterBuilder writer(builder);
-        spdlog::info("Writing Config to pacco.config.json");
-        std::ofstream config("pacco.config.json");
+        spdlog::debug("Writing Config to alpacco.config.json");
+        std::ofstream config("alpacco.config.json");
         config << Json::writeString(writer, root);
         config.close();
         
     } catch (...) {
         spdlog::error("Creting config file returned error!");
     }
-    spdlog::info("Creating config file done");
+    spdlog::debug("Creating config file done");
 
 }
